db776f7aba17fb5e40687d0668340def15fd278b
ivaldeolivas <ivaldeolivas@gmail.com>
2016-03-31 07:10:39 +0200

    Changed conditions to if

--- memoryGame.playground/Contents.swift
+++ memoryGame.playground/Contents.swift
@@ -13,13 +13,14 @@
 for i in 1...100 {
     if (i % 5 == 0){
         print("# \(i) Bingo!!!")
-        
-    } else if (i % 2 == 0) {
+    }
+    
+    if (i % 2 == 0) {
         print("# \(i) par!!!")
-        
-    } else if (i % 2 != 0) {
+    }
+    
+    if (i % 2 != 0) {
         print("# \(i) impar!!!")
-        
     }
     
     if (i >= 30 && i <= 40) {
